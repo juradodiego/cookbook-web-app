2447e576d662d4345328dcd81906c5b074cdcc20
Thalsey <tmh104@pitt.edu>
2022-11-06 15:40:24 -0500

    Finally figured out how to configure the test stuff (I THINK ???) Should be good going forward. When adding test cases just put the in the test folder (src/main/tests)

---
+++ cookbook/src/main/tests/CustomRecipeRepositoryImplTest.java
@@ -0,0 +1,26 @@
+package tests;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import junit.framework.TestCase;
+
+class CustomRecipeRepositoryImplTest extends TestCase {
+
+	@BeforeEach
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	@AfterEach
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	@Test
+	void testFilteredSearch() {
+		fail("Not yet implemented");
+	}
+
+}
