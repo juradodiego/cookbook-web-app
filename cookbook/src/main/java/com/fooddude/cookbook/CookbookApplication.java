e0f7bda767c905bae0f41d7b8f1c9f8f54534e91
Diego Jurado <diego.jurado@pitt.edu>
2022-11-11 11:37:23 -0500

    Updated Application file

--- cookbook/src/main/java/com/fooddude/cookbook/CookbookApplication.java
+++ cookbook/src/main/java/com/fooddude/cookbook/CookbookApplication.java
@@ -1,4 +1,4 @@
-package cookbook.cookbook;
+package com.fooddude.cookbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
