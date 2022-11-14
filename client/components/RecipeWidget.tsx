cac3f49b0d05881d9659f02c28d4012b3951bf6a
Ben Schuman <ben@jeffschuman.com>
2022-11-14 01:22:16 -0500

    rounded top of image on widget

--- client/components/RecipeWidget.tsx
+++ client/components/RecipeWidget.tsx
@@ -10,7 +10,7 @@ export default function RecipeWidget({ id, name, imageUrl }: componentProps) {
   return (
     <Link href={`/recipes/${id}`}>
       <div className="bg-white rounded-md w-56 flex flex-col transition duration-100 hover:scale-105">
-        <img src={imageUrl} className="w-56 h-40 object-cover" />
+        <img src={imageUrl} className="w-56 h-40 object-cover rounded-t-md" />
         <div className="w-56 h-28">
           <p className="text-2xl text-gray-800 p-3 font-serif">{name}</p>
         </div>
